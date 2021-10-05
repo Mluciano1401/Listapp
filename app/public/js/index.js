86b3a1f4c38f65527b5cf6e5128dc54c47fb7bc1
Mluciano1401 <74382484+Mluciano1401@users.noreply.github.com>
2021-10-05 15:37:44 -0400

    add files

---
+++ app/public/js/index.js
@@ -0,0 +1,40 @@
+"use strict";
+var bAdd = document.querySelector('#bAdd');
+var inputTitle = document.querySelector('#title');
+var inputCost = document.querySelector('#cost');
+var inputCurrency = document.querySelector('#currency');
+var expenses = new Expenses('USD');
+bAdd.addEventListener('click', function (e) {
+    if (inputTitle.value != '' && inputCost.value != '' && !isNaN(parseFloat(inputCost.value))) {
+        var title = inputTitle.value;
+        var cost = parseFloat(inputCost.value);
+        var currency = (inputCurrency.value);
+        expenses.add({ title: title, cost: { number: cost, currency: currency } });
+        render();
+    }
+    else {
+        alert('Completa los datos correctamente');
+    }
+});
+function render() {
+    var html = '';
+    expenses.getItems().forEach(function (item) {
+        var id = item.id, title = item.title, cost = item.cost;
+        html += "\n        <div class=\"item\">\n        <div>\n        <span class=\"currency\">" + cost.currency + "</span>\n        " + cost.number + "\n        </div>\n        <div>" + title + "</div>\n        <div>\n        <button class=\"bEliminar\" data-id=\"" + id + "\">Eliminar</button>\n        </div>\n        </div>\n        ";
+    });
+    $('#items').innerHTML = html;
+    $('#display').textContent = expenses.getTotal();
+    $$('.bEliminar').forEach(function (bEliminar) {
+        bEliminar.addEventListener('click', function (e) {
+            var id = e.target.getAttribute('data-id');
+            expenses.remove(parseInt(id));
+            render();
+        });
+    });
+}
+function $(selector) {
+    return document.querySelector(selector);
+}
+function $$(selector) {
+    return document.querySelectorAll(selector);
+}
